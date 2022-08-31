e1871e3ab36a619052ea31f942a4b6465340e541
Chaytan Arora <chaytanarora@gmail.com>
2022-08-31 18:02:28 +0530

    done problem 8.5: find town judge

---
+++ Week-8/problem_8_5.js
@@ -0,0 +1,27 @@
+// Find the town judge
+
+
+function findJudge(n, trust) {
+    if (!Array.isArray(trust)) throw 'Invalid trust array';
+    if (n < 1) return -1;
+
+    let output = new Array(n + 1).fill(0);
+    for (let i = 0; i < trust.length; i++) {
+        output[trust[i][0]]--;
+        output[trust[i][1]]++;
+    }
+
+    for (let j = 1; j <= n; j++) {
+        if (output[j] === n - 1) return j;
+    }
+    return -1;
+}
+
+
+try {
+    let trust = [[1, 3], [1, 4], [2, 3], [2, 4], [4, 3]];
+    let result = findJudge(4, trust)
+    console.log(result);
+} catch (e) {
+    console.warn(e);
+}
