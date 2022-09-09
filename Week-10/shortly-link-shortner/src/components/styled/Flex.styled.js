8c0e43f97a0e691058c6ef4138ea916685f9f798
Chaytan Arora <chaytanarora@gmail.com>
2022-09-10 02:49:06 +0530

    implemented link shortening functionality

--- Week-10/shortly-link-shortner/src/components/styled/Flex.styled.js
+++ Week-10/shortly-link-shortner/src/components/styled/Flex.styled.js
@@ -2,41 +2,24 @@ import styled from "styled-components";
 
 
 export const Flex = styled.div`
+	display: flex;
+	padding: ${({ padding }) => padding || "50px 0"};
+	justify-content: space-between;
+	align-items: center;
+	flex-direction: ${({ direction }) => direction || "row"};
+	position: relative;
+	top: ${({ top }) => top};
 
-    display: flex;
-    padding: ${({ padding }) => padding || '50px 0'};
-    justify-content: space-between;
-    align-items: center;
-    flex-direction: ${({ direction }) => direction || 'row'} ;
-    position: relative;
-    top: ${({ top }) => top};
-    
-    & h1{
-        font-size: 4.5rem;
-        font-weight: 800;
-        line-height: 1;
-        color: ${({ theme }) => theme.colors.veryDarkBlue};
-        margin-bottom: 0;
-    }
+	& h1 {
+		font-size: 4.5rem;
+		font-weight: 800;
+		line-height: 1;
+		color: ${({ theme }) => theme.colors.veryDarkBlue};
+		margin-bottom: 0;
+	}
 
-    & p{
-        font-size: 1.2rem;
-        margin-bottom: 40px;
-        
-    }
-
-    & a{
-        text-decoration: none;
-        font-size: 1.3rem;
-        padding: 1rem;
-    }
-
-    & .og-link{
-        flex-grow: 1;
-        color: ${({theme}) => theme.colors.veryDarkBlue};
-    }
-
-    & .result-link{
-        color: ${({theme}) => theme.colors.cyan};
-    }
-`
+	& p {
+		font-size: 1.2rem;
+		margin-bottom: 40px;
+	}
+`;
