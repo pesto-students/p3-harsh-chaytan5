f38d3f2c5ca051932267a0cd3fbedde364799039
Chaytan Arora <chaytanarora@gmail.com>
2022-09-22 14:25:48 +0530

    added add step and reset functionality in app.js

--- Week-11/step-counter/src/App.js
+++ Week-11/step-counter/src/App.js
@@ -1,11 +1,20 @@
-import './App.css';
+import "./App.css";
+import { useSelector, useDispatch } from "react-redux";
+import * as actions from "./actions";
 
 function App() {
-  return (
-    <div className="App">
-      <h1>Step Counter</h1>
-    </div>
-  );
+	const stepCount = useSelector((state) => state);
+	const dispatch = useDispatch();
+
+	return (
+		<div className="App">
+			<h1>Step Counter</h1>
+			<h3>You have walked {stepCount} steps today!</h3>
+			<button onClick={() => dispatch(actions.addStep())}>Add a Step</button>
+			<br />
+			<button onClick={() => dispatch(actions.reset())}>Reset</button>
+		</div>
+	);
 }
 
 export default App;
