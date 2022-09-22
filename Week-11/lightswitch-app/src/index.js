b33c5a6b927a33a04e0c1dde9f908b4717224666
Chaytan Arora <chaytanarora@gmail.com>
2022-09-22 11:42:48 +0530

    created store in index.js using lightswitch reducer

--- Week-11/lightswitch-app/src/index.js
+++ Week-11/lightswitch-app/src/index.js
@@ -2,12 +2,18 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import { createStore } from 'redux';
+import { Provider } from 'react-redux'
+import lightSwitchReducer from './reducers';
+
+const store = createStore(lightSwitchReducer);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
+  <Provider store={store}>
     <App />
-  </React.StrictMode>
+  </Provider>
+
 );
 
 
