be0cdf4ab5451b2ddb02bc9cf804206268e4a1a2
Chaytan Arora <chaytanarora@gmail.com>
2022-09-22 14:19:58 +0530

    created store and added provider in index.js

--- Week-11/step-counter/src/index.js
+++ Week-11/step-counter/src/index.js
@@ -1,12 +1,16 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import { Provider } from "react-redux";
+import { createStore } from "redux";
+import stepCounterReducer from "./reducers";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const store = createStore(stepCounterReducer);
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+	<Provider store={store}>
+		<App />
+	</Provider>
 );
-
