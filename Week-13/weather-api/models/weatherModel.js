44dab38d789805b47c51799ce09007894dc345c6
Chaytan Arora <chaytanarora@gmail.com>
2022-10-20 21:17:44 +0530

    added getLatLong function in models to get latitude and longitude of a location

--- Week-13/weather-api/models/weatherModel.js
+++ Week-13/weather-api/models/weatherModel.js
@@ -2,16 +2,24 @@ import { fetch } from "node-fetch";
 
 const API_KEY = "170cec7803bff371fc0fcf1e9126a03b";
 
+async function getLatLong(city) {
+	const latLongRes = await fetch(
+		`http://api.openweathermap.org/geo/1.0/direct?q=${city}&limit=1&appid=${APPID}`
+	);
+
+	const latLongData = await latLongRes.json();
+
+	if (!latLongData.length) {
+		throw new Error("ENOENT");
+	}
+}
+
 async function getCurrentWeather(lat, lon) {
 	const weatherRes = await fetch(
 		`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric`
 	);
 
 	const weatherData = await weatherRes.json();
-
-	if (!weatherData.length) {
-		throw new Error("ENOENT");
-	}
 
 	return weatherData;
 }
